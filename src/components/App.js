6dc92158fd7031cfd85bf036806b602485e41938
nbond211 <bond.n@husky.neu.edu>
2017-05-04 21:33:12 -0400

    implement autoplay feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import autoBind from 'react-autobind';
 import '../styles/App.css';
 import photoData from '../photo-data';
 import TopBar from './top-bar';
@@ -12,6 +13,7 @@ const maxIndex = photos.length - 1;
 class App extends Component {
   constructor(props) {
     super(props);
+    autoBind(this);
     this.state = {
       index: 34
     };
@@ -31,18 +33,61 @@ class App extends Component {
       const newIndex = currentIndex - 1;
       this.setState({index: newIndex});
     }
+    
+  }
+
+  clickNext = () =>{
+    const {intervalId} = this.state;
+    this.clearInterval(intervalId);
+    this.setState({isPlaying: false});
+    this.nextPhoto();
+  }
+
+  clickPrev = () =>{
+    const {intervalId} = this.state;
+    this.clearInterval(intervalId);
+    this.setState({isPlaying: false});
+    this.prevPhoto();
   }
 
   autoPlay = () => {
-    window.setInterval(function(){
+    const intervalId = window.setInterval(() => {
       this.nextPhoto();
-}, 1000);
+    }, 1500);
+    this.setState({intervalId, isPlaying: true});
+  }
+
+  pause = () => {
+    const {intervalId} = this.state;
+    this.clearInterval(intervalId);
+    this.setState({isPlaying: false});
+  }
+
+  clearInterval = intervalId => {
+    if (intervalId) {
+      window.clearInterval(intervalId);
+    }
   }
 
   render() {
-    const {index} = this.state;
+    const {index, isPlaying} = this.state;
     const photo = photoData[photos[index]];
     const {colors, dateTime, gpsLatitude, gpsLatitudeRef, gpsLongitude, gpsLongitudeRef} = photo;
+
+    let playButton;
+    if (isPlaying) {
+      playButton = (
+        <button onClick={this.pause} className="button">
+          <i className="material-icons button-icon">pause_circle_outline</i>
+        </button>
+      );
+    } else {
+      playButton = (
+        <button onClick={this.autoPlay} className="button">
+          <i className="material-icons button-icon">play_circle_outline</i>
+        </button>
+      )
+    }
 
     return (
       <div className="container">
@@ -50,12 +95,7 @@ class App extends Component {
           <button className="button">
             <i className="material-icons button-icon">info_outline</i>
           </button>
-          <button className="button">
-            <i className="material-icons button-icon">pause_circle_outline</i>
-          </button>
-          <button className="button">
-            <i className="material-icons button-icon">play_circle_outline</i>
-          </button>
+          {playButton}  
           <button className="button">
             <i className="material-icons button-icon">view_week</i>
           </button>
@@ -71,9 +111,9 @@ class App extends Component {
         gpsLongitudeRef={gpsLongitudeRef}
         />
         <div className="bottom-row">
-          <PrevButton prevPhoto={this.prevPhoto} active={index > 0}/>
+          <PrevButton prevPhoto={this.clickPrev} active={index > 0}/>
           <ColorSwatches colors={colors}/>
-          <NextButton nextPhoto={this.nextPhoto} active={index < maxIndex}/>
+          <NextButton nextPhoto={this.clickNext} active={index < maxIndex}/>
         </div>
       </div>
     );
